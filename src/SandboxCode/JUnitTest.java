5d2e52cbc6e430e404801ffc13552b301b182885
Anna <Anna@AnnaLaptop>
2016-06-08 15:01:32 -0600

    Junit examples being added

---
+++ src/SandboxCode/JUnitTest.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SandboxCode;
+
+/**
+ *
+ * @author Jae Dillree
+ */
+public class JUnitTest {
+
+    public int addTest(int one, int two) {
+    return one + two;
+    }
+}
