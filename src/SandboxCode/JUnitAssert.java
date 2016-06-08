c74ed735b392602cba42b8bfc578d148726ab358
Anna <Anna@AnnaLaptop>
2016-06-08 16:22:54 -0600

    Junit examples being added

---
+++ src/SandboxCode/JUnitAssert.java
@@ -0,0 +1,11 @@
+
+package SandboxCode;
+
+/**
+ * @author Jae Dillree
+ */
+public class JUnitAssert {
+    public int add(int one, int two){
+        return one + two;
+    }
+}
---
+++ test/SandboxCode/JUnitTestSuite.java
@@ -0,0 +1,17 @@
+
+package SandboxCode;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @author Jae Dillree
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({SandboxCode.JUnitAssertTest.class, SandboxCode.ADD OTHER TEST HERE.class, SandboxCode.ADD OTHER TEST HERE.class})
+public class JUnitTestSuite {
+}
